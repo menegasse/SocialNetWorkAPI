606c9927948ff32d060bbd903650ab0a5980724b
menegasse <menegassehugo12@gmail.com>
2021-07-15 08:58:29 -0300

    adding environment variable

--- scripts/install-project.sh
+++ scripts/install-project.sh
@@ -5,4 +5,5 @@ source ../env/bin/activate
 pip install -r ../requirements.txt
 python ../manage.py makemigrations
 python ../manage.py migrate
-python ../manage.py runserver
+source ./start.sh
+
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 source ../env/bin/activate
+source ../.env #if .env file don't exist you need to create it in root directory of project and export the SECRET_KEY
 python ../manage.py runserver
